7b2104ab345672c538f57b4749d3eaf28af64f9b
Ivan <rfhjykgdfrgghf@gmail.com>
2024-05-12 15:42:19 +0300

    Add files via upload

---
+++ price.js
@@ -0,0 +1,16 @@
+export const PRICE={
+    "SD3":6.5,
+    "SD3 Turbo":4,
+    "Core":3,
+    "SDXL 1.0":0.6,
+    "SD 1.6":1,
+    "Creative Upscaler":25,
+    "ESRGAN":0.2,
+    "Search and Replace":4,
+    "Inpaint":3,
+    "Outpaint":4,
+    "Remove Background":2,
+    "Structure":3,
+    "Sketch":3,
+    "Stable Video":20
+}
